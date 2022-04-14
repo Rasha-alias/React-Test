e44d7d185de61101b4e4d84bafa83d55358a249d
rasha-alias <rashaziyad000@gmail.com>
2022-04-14 22:34:47 +0200

    Push both folder to GiHhub Repo

---
+++ react-test-basic/src/components/componentA/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+export default class ComponentA extends Component {
+    render() {
+        return (
+            <div className={"component"}>
+                <span className={"component_title"}> Component A </span>
+            </div>
+        )
+    }
+}
---
+++ react-test-fetch/src/App.js
@@ -0,0 +1,15 @@
+import { ComponentA } from "./components/componentA";
+import { ComponentB } from "./components/componentB";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <ComponentA />
+      <ComponentB />
+    </div>
+  );
+}
+
+export default App;
